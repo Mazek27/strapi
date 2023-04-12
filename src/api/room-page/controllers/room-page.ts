84c0d79bedfda3f3df8a943ab8228dc72643ee07
Karol Łukaszuk <mazekpl@gmail.com>
2023-04-12 06:55:53 +0200

    Innit Commit

--- src/api/room-page/controllers/room-page.ts
+++ src/api/room-page/controllers/room-page.ts
@@ -1,7 +1,40 @@
-/**
- * room-page controller
- */
+const { sanitize } = require('@strapi/utils');
 
-import { factories } from '@strapi/strapi'
+module.exports = {
+  find: async (ctx) => {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.entityService.search('api::room-page.room-page', ctx.query);
+    } else {
+      entities = await strapi.entityService.findMany('api::room-page.room-page', ctx.query);
+    }
 
-export default factories.createCoreController('api::room-page.room-page');
+    const model = strapi.getModel('room-page');
+    return entities.map((entity) => sanitize(entity, { model }));
+  },
+
+  findOneByPlaceAndRoomId: async (ctx) => {
+    const { place_id, room_id } = ctx.query;
+
+    if (!place_id || !room_id) {
+      return ctx.badRequest('Both place_id and room_id must be provided.');
+    }
+
+    console.log({ place_id, room_id })
+
+    const entity = await strapi.entityService.findOne('api::room-page.room-page',1, {
+      where: [
+        { place_id: place_id },
+        { room_id: room_id },
+      ],
+      populate: 'deep'
+    });
+
+    if (!entity) {
+      return ctx.notFound('No room page found with the given place_id and room_id.');
+    }
+
+    const sanitizedEntity = await sanitize.contentAPI.output(entity);
+    return { data: sanitizedEntity }
+  },
+};
--- src/api/room-page/services/room-page.ts
+++ src/api/room-page/services/room-page.ts
@@ -1,7 +1,29 @@
-/**
- * room-page service
- */
+module.exports = {
+  async find(params, populate) {
+    return await strapi.entityService.findMany('api::room-page.room-page', params, populate);
+  },
 
-import { factories } from '@strapi/strapi';
+  async findOne(params, populate) {
+    return await strapi.entityService.findOne('api::room-page.room-page', params, populate);
+  },
 
-export default factories.createCoreService('api::room-page.room-page');
+  async search(params) {
+    return await strapi.entityService.search('api::room-page.room-page', params);
+  },
+
+  async count(params) {
+    return await strapi.entityService.count('api::room-page.room-page', params);
+  },
+
+  async create(data) {
+    return await strapi.entityService.create('api::room-page.room-page', data);
+  },
+
+  async update(params, data) {
+    return await strapi.entityService.update('api::room-page.room-page', params, data);
+  },
+
+  async delete(params) {
+    return await strapi.entityService.delete('api::room-page.room-page', params);
+  },
+};
